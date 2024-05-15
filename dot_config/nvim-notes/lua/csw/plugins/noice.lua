2716fefbcb026f0ae83df924244542627925f887
Shaun Garwood <shaun.garwood@companycam.com>
2024-05-15 13:38:13 -0600

    new notes setup

---
+++ dot_config/nvim-notes/lua/csw/plugins/noice.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/noice.nvim",
+  enabled = true,
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  }
+}
