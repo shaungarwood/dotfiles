5c7d58a3493e2bff3bd4d50a1ffb6f03de51e3e0
Shaun Garwood <shaun.garwood@companycam.com>
2024-05-15 15:34:29 -0600

    Cleaned up .zshrc

--- run_once_before_zshrc.sh
+++ run_once_before_zshrc.sh
@@ -1,27 +1,56 @@
 #!/usr/bin/env bash
 # This script is run once before zshrc is sourced
 
-sudo apt-get update
-sudo apt-get install -y curl git gpg gcc make zlib1g-dev libffi-dev libssl-dev libyaml-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
-sudo apt-get install -y zsh
+# Install/setup zsh and oh-my-zsh
+# if the current shell is not zsh
+if [[ $SHELL != *"zsh"* ]]; then
+  OS=$(uname)
 
-# Set zsh as the default shell
-chsh -s $(which zsh)
+  case $OS in
+    "Darwin")
+      # Mac specific commands here
+      echo "Running on Mac"
+      ;;
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    "Linux")
+      # Linux specific commands here
+      echo "Running on Linux"
+      sudo apt-get update
+      sudo apt-get install -y curl git gpg gcc make zlib1g-dev libffi-dev libssl-dev libyaml-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
+      sudo apt-get install -y zsh
+      ;;
+  esac
+
+  # Set zsh as the default shell
+  chsh -s $(which zsh)
+
+  # Install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-source ~/.asdf/asdf.sh
-asdf plugin add nodejs
-asdf install nodejs latest
-asdf global nodejs latest
+if ! command -v asdf >/dev/null; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+  source ~/.asdf/asdf.sh
+fi
 
-asdf plugin add ruby
-asdf install ruby latest
-asdf global ruby latest
+# Install nodejs
+if ! which node | grep -q 'asdf'; then
+  asdf plugin add nodejs
+  asdf install nodejs latest
+  asdf global nodejs latest
+fi
 
-asdf plugin add python
-asdf install python latest
-asdf global python latest
+# Install ruby
+if ! which ruby | grep -q 'asdf'; then
+  asdf plugin add ruby
+  asdf install ruby latest
+  asdf global ruby latest
+fi
+
+# Install python
+if ! which python | grep -q 'asdf'; then
+  asdf plugin add python
+  asdf install python latest
+  asdf global python latest
+fi
