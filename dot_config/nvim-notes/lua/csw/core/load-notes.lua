7872f8ba6c53aa5d763c932591b0a3da53fe58e6
Shaun Garwood <shaun.garwood@companycam.com>
2024-05-20 13:15:21 -0600

    new note command

--- dot_config/nvim-notes/lua/csw/core/load-notes.lua
+++ dot_config/nvim-notes/lua/csw/core/load-notes.lua
@@ -1,2 +1,13 @@
 vim.api.nvim_command('cd ~/repos/notes')
 vim.api.nvim_command('e README.md')
+
+function create_and_open_file()
+  vim.api.nvim_command('NvimTreeOpen')
+  local filename = vim.fn.input('Enter filename: ')
+  if filename ~= '' then
+    vim.api.nvim_command('edit ' .. filename)
+    vim.api.nvim_command('write')
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>nn', ':lua create_and_open_file()<CR>', {noremap = true, silent = true})
