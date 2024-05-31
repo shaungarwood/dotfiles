f3a29e44bf38c421a1f1624d64293f8ea16de3a6
Shaun Garwood <shaun.garwood@companycam.com>
2024-05-31 08:15:08 -0600

    add harpoon and don't save special buffers

--- dot_config/nvim/lua/csw/plugins/auto-save.lua
+++ dot_config/nvim/lua/csw/plugins/auto-save.lua
@@ -5,6 +5,15 @@ return {
   opts = {
     execution_message = {
       enabled = false
-    }
+    },
+    condition = function(buf)
+      local fn = vim.fn
+
+      -- don't save for special-buffers
+      if fn.getbufvar(buf, "&buftype") ~= '' then
+        return false
+      end
+      return true
+    end
   }
 }
---
+++ dot_config/nvim/lua/csw/plugins/harpoon.lua
@@ -0,0 +1,26 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon.setup({})
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    -- keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    -- keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+    -- keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+    -- keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    -- keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    -- keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+  end,
+}
+
+
