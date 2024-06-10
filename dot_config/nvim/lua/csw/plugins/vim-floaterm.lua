579f65eaf1e2d308e1c15da90efa62b6ad5ac539
Shaun Garwood <shaun.garwood@gmail.com>
2024-06-09 20:16:39 -0600

    sweeeeet floating term

---
+++ dot_config/nvim/lua/csw/plugins/vim-floaterm.lua
@@ -0,0 +1,24 @@
+return {
+  "voldikss/vim-floaterm",
+  config = function()
+    local function new_floaterm(keymap, cmd, wait_for_input)
+      cmd = cmd or '' -- If no command is provided, use an empty string
+      local command = ':FloatermNew --height=0.9 --width=0.9 --wintype=float --autoclose=2 ' .. cmd
+      if wait_for_input then
+        command = command .. '; read'
+      end
+      command = command .. '<CR>'
+      vim.api.nvim_set_keymap('n', keymap, command, {noremap = true, silent = true})
+    end
+
+    -- Lazygit
+    new_floaterm('<leader>lg', 'lazygit')
+
+    -- Rspec
+    local filename = vim.fn.expand('%:p')
+    new_floaterm('<leader>rs', 'rspec ' .. filename, true)
+
+    -- Just a generic terminal
+    new_floaterm('<leader>tn')
+  end,
+}
