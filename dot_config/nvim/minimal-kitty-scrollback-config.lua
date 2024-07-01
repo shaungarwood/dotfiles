ea14d604237683fef754c57701acb70222f9f9f7
Shaun Garwood <shaun.garwood@gmail.com>
2024-06-30 18:08:51 -0600

    big rework of kitty config

--- dot_config/nvim/minimal-kitty-scrollback-config.lua
+++ dot_config/nvim/minimal-kitty-scrollback-config.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = ","
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- vim.opt.setlinebreak = true             -- wrap long lines at whitespace
+vim.wo.wrap = true
+vim.wo.linebreak = true
+
 vim.opt.mouse = ''                      -- disable mouse
 vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
