a91967942254b8aa07336a5895e8a51ef8f4aa34
Shaun Garwood <shaun.garwood@companycam.com>
2024-10-16 13:16:01 -0600

    grug

---
+++ dot_config/nvim/lua/csw/plugins/grug_far.lua
@@ -0,0 +1,27 @@
+return  {
+  'MagicDuck/grug-far.nvim',
+  config = function()
+    require('grug-far').setup({
+      preview = {
+        border = 'rounded',
+      },
+    });
+  end,
+  keys = {
+    { "\\", "<cmd>GrugFar<CR>", { noremap = true } },
+    {
+      "{",
+      function()
+        local grug = require('grug-far')
+        grug.open({
+          prefills = {
+            -- search = "word"
+            search = vim.fn.expand("<cword>")
+          }
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search for word under cursor",
+    },
+  }
+}
