bd591e9c102a400ea1be1efa7aa266d669fb540c
Shaun Garwood <shaun.garwood@gmail.com>
2025-12-12 23:24:19 +0000

    newer asdf

--- run_once_before_asdfrc.sh
+++ run_once_before_asdfrc.sh
@@ -14,8 +14,28 @@ fi
 
 # Install asdf
 if ! command -v asdf >/dev/null; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-  source ~/.asdf/asdf.sh
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.18.0
+  cd ~/.asdf
+  
+  # Build the Go-based asdf (requires Go)
+  if command -v go >/dev/null; then
+    make
+    cp ./asdf ./bin/asdf
+  else
+    # If no Go available, install old version first to bootstrap golang
+    git checkout v0.14.0
+    source ~/.asdf/asdf.sh
+    asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+    asdf install golang latest
+    asdf global golang latest
+    echo "golang $(asdf current golang | awk '{print $2}')" >> ~/.asdf/.tool-versions
+    
+    # Now build the new version
+    git checkout v0.18.0
+    ~/.asdf/installs/golang/*/go/bin/go build -ldflags='-s' -o=./asdf ./cmd/asdf
+    cp ./asdf ./bin/asdf
+  fi
+  cd -
 fi
 
 # Install nodejs
