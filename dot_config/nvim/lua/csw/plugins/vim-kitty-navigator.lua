be9776057fb0f7bfc3dd310c7f700ebad4def970
Shaun Garwood <shaun.garwood@gmail.com>
2026-03-10 19:23:29 -0600

    more zellij stuff

--- dot_config/nvim/lua/csw/plugins/vim-kitty-navigator.lua
+++ dot_config/nvim/lua/csw/plugins/vim-kitty-navigator.lua
@@ -1,3 +1,10 @@
 return {
-  "knubie/vim-kitty-navigator",
+  "mrjones2014/smart-splits.nvim",
+  config = function()
+    require("smart-splits").setup()
+    vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+    vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+    vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+    vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+  end,
 }
