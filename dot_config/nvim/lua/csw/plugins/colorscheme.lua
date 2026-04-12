a2fb4179ed5b5518c75c2f371ba51c01494a2285
Shaun Garwood <shaun.garwood@gmail.com>
2026-04-12 14:36:16 -0600

    T14 Gen 5: wofi, volume 150%, waybar temp segment, nvim persistent light theme toggle

--- dot_config/nvim/lua/csw/plugins/colorscheme.lua
+++ dot_config/nvim/lua/csw/plugins/colorscheme.lua
@@ -3,10 +3,18 @@ return {
     "bluz71/vim-nightfly-guicolors",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
+      local flag = vim.fn.expand("~/.config/nvim/.light-theme")
+
       vim.cmd([[colorscheme nightfly]])
-      vim.cmd([[command! Lighttheme set background=light]])
-      vim.cmd([[command! Darktheme set background=dark]])
+
+      -- Apply persisted theme preference on startup
+      if vim.fn.filereadable(flag) == 1 then
+        vim.cmd([[set background=light]])
+      end
+
+      -- Toggle commands that persist the preference
+      vim.cmd([[command! Lighttheme lua vim.cmd('set background=light'); vim.fn.writefile({}, vim.fn.expand('~/.config/nvim/.light-theme'))]])
+      vim.cmd([[command! Darktheme lua vim.cmd('set background=dark'); vim.fn.delete(vim.fn.expand('~/.config/nvim/.light-theme'))]])
     end,
   }
 }
